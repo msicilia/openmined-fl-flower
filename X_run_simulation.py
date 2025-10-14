8ed08f66a25f5726e949b8a68b6b58c8fcb67280
Miguel-Angel Sicilia <msicilia@gmail.com>
2025-10-14 14:29:24 +0200

    Fixed mocks for simulation

--- X_run_simulation.py
+++ X_run_simulation.py
@@ -1,21 +1,30 @@
 from pathlib import Path
 import os
-
+import syft_flwr
 
 SYFT_FLWR_PROJECT_PATH = Path("./simple-classifier")
 assert SYFT_FLWR_PROJECT_PATH.exists()
 
 RUN_SIMULATION = True
 
-if RUN_SIMULATION:
-    stream = os.popen(f"uv run flwr run {SYFT_FLWR_PROJECT_PATH}")
-    output = stream.read()
-    print(output)
-
-
+try:
+    os.remove(SYFT_FLWR_PROJECT_PATH / "simple-classifier" / "__pycache__")  # clean before submitting
+except OSError:
+    pass
 # !rm -rf {SYFT_FLWR_PROJECT_PATH / "fl_diabetes_prediction" / "__pycache__"}  # clean before submitting
 # !rm -rf {"../weights/"}
 
+mock_paths = []
+for client in range(1, 3):
+    #dataset = client.dataset.get(name=SYFTBOX_DATASET_NAME)
+    mock_paths.append(f"./data/node_{client}/mock")
+
+if RUN_SIMULATION:
+    print(f"running syft_flwr simulation with mock paths: {mock_paths}")
+    syft_flwr.run(SYFT_FLWR_PROJECT_PATH, mock_paths)
+
+
 # if RUN_SIMULATION:
-#     print(f"running syft_flwr simulation with mock paths: {mock_paths}")
-#     syft_flwr.run(SYFT_FLWR_PROJECT_PATH, mock_paths)
+#     stream = os.popen(f"uv run flwr run {SYFT_FLWR_PROJECT_PATH}")
+#     output = stream.read()
+#     print(output)
